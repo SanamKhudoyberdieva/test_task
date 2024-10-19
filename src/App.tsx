df3562f93c002b1a219d1638010c1c6afa9d6c83
SanamKhudoyberdieva <khudoyberdievasanam@gmail.com>
2024-10-19 13:36:23 +0500

    style updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
-import './styles/index.css';
-import './styles/media.css'
+import './assets/style/index.css';
+import './assets/style/media.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import App from './App';
 import React from 'react';
-import './styles/index.css'; 
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
