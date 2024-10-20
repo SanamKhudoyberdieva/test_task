5980b5101f0358bb3c8e129086d5f35a6a66d533
SanamKhudoyberdieva <khudoyberdievasanam@gmail.com>
2024-10-20 19:41:19 +0500

    hero card update

--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -11,15 +11,19 @@ interface HeroCardProps {
 const HeroCard: React.FC<HeroCardProps> = ({ id, name, height, mass, onClick }) => {
   return (
     <div className="ss-hero-item" onClick={onClick} role="button" tabIndex={0} aria-label={`View details for ${name}`}>
-      <img 
-        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} 
-        alt={`${name}'s image`} 
-        className="ss-hero-image" 
-      />
-      <h3 className="ss-hero-name">{name}</h3>
-      <p className="ss-hero-details">
-        Height: <span>{height}</span> | Mass: <span>{mass}</span>
-      </p>
+      <div className="ss-hero-image-container">
+        <img 
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} 
+          alt={`${name}'s image`} 
+          className="ss-hero-image" 
+        />
+        <div className="ss-hero-info">
+          <h3 className="ss-hero-name">{name}</h3>
+          <p className="ss-hero-details">
+            Height: <span>{height}</span> | Mass: <span>{mass}</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
